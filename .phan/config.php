db8435cf3e7a15c5d2a238b0fea177e8ea5f2455
Daimona Eaytoy <daimona.wiki@gmail.com>
2019-03-30 19:53:47 +0100

    skeleton

---
+++ .phan/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'directory_list' => [
+        'includes',
+    ],
+
+    'file_list' => [
+		'run.php',
+	]
+
+    'exclude_analysis_directory_list' => [
+        'vendor/'
+    ],
+];
