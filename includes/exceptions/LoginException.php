86c8ddcfa13cc3f373bd6376659ea29f4372d216
Daimona Eaytoy <daimona.wiki@gmail.com>
2019-04-03 13:49:03 +0200

    leftovers

--- includes/exceptions/LoginException.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace BotRiconferme\Exceptions;
-
-class LoginException extends \Exception {
-}
--- includes/exceptions/TaskException.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace BotRiconferme\Exceptions;
-
-class TaskException extends \Exception {
-}
